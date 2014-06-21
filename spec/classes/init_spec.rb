4cc47c03a3704ff3ecee50f5d6b1204cdcfebae5
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-06-21 12:54:03 +1000

    Review unit tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,168 +4,109 @@ describe 'jira' do
   let(:title) {'jira'}
   let(:facts) { {:postgres_default_version => '9.2', :operatingsystem => 'Debian', :osfamily => 'Debian'} }
 
-  describe 'standard installation' do
+  describe 'by default' do
     let(:params) { {} }
 
-    it { should contain_archive('atlassian-jira-6.2') }
-    it { should contain_service('jira').with_ensure('running').with_enable(true) }
+    specify { should contain_archive('atlassian-jira-6.2') }
+    specify { should contain_service('jira').with_ensure('running').with_enable(true) }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/protocol="AJP\/1.3"/) }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="8009"/) }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/JIRA_MAX_PERM_SIZE=384m/) }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').without_content(/-Datlassian.plugins.enable.wait=/) }
+    specify { should contain_class("jira::package").with_require('Package[sun-java6-jdk]') }
+    specify { should contain_cron('cleanup-jira-export').with_command('find /data/jira/export/ -name "*.zip" -type f -mtime +7 -delete') }
+    specify { should contain_file('/data/jira/dbconfig.xml') }
   end
 
-  describe 'custom version' do
+  describe 'with version => 1.0.0' do
     let(:params) { {:version => '1.0.0'} }
 
-    it { should contain_archive('atlassian-jira-1.0.0') }
+    specify { should contain_archive('atlassian-jira-1.0.0') }
   end
 
-  describe 'disable service' do
+  describe 'with disable => true' do
     let(:params) { {:disable => true} }
 
-    it { should contain_service('jira').with_ensure('stopped').with_enable(false) }
+    specify { should contain_service('jira').with_ensure('stopped').with_enable(false) }
   end
 
   describe 'default HTTP address and port' do
     let(:params) { {:protocols => ['http']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/address="127.0.0.1"/)
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="8080"/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/address="127.0.0.1"/) }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="8080"/) }
   end
 
   describe 'empty HTTP address' do
     let(:params) { {:http_address => '', :protocols => ['http']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/) }
   end
 
   describe 'wildcard HTTP address' do
     let(:params) { {:http_address => '*', :protocols => ['http']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/) }
   end
 
   describe 'custom HTTP address' do
     let(:params) { {:http_address => '1.2.3.4', :protocols => ['http']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/address="1.2.3.4"/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/address="1.2.3.4"/) }
   end
 
   describe 'custom HTTP port' do
     let(:params) { {:http_port => '80', :protocols => ['http']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="80"/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="80"/) }
   end
 
   describe 'custom AJP port' do
     let(:params) { {:ajp_port => 1234, :protocols => ['ajp']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="1234"/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="1234"/) }
   end
 
   describe 'empty AJP address' do
     let(:params) { {:ajp_address => '', :protocols => ['ajp']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/) }
   end
 
   describe 'wildcard AJP address' do
     let(:params) { {:ajp_address => '*', :protocols => ['ajp']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').without_content(/address=/) }
   end
 
   describe 'custom AJP address' do
     let(:params) { {:ajp_address => '1.2.3.4', :protocols => ['ajp']} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/protocol="AJP\/1.3"/)
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/address="1.2.3.4"/)
-    end
-  end
-
-  describe 'default AJP address and port' do
-    let(:params) { {} }
-
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/protocol="AJP\/1.3"/)
-      should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/port="8009"/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/protocol="AJP\/1.3"/) }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/conf/server.xml').with_content(/address="1.2.3.4"/) }
   end
 
   describe 'custom java opts' do
     let(:params) { {:java_opts => '-Xms512m -Xmx1024m'} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/-Xms512m -Xmx1024m/)
-    end
-  end
-  describe 'by default' do
-    let(:params) { {} }
-
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/JIRA_MAX_PERM_SIZE=384m/)
-    end
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/-Xms512m -Xmx1024m/) }
   end
 
   describe 'with java_permgen => 256m' do
     let(:params) { {:java_permgen => '256m'} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/JIRA_MAX_PERM_SIZE=256m/)
-    end
-  end
-
-  describe 'depends on sun-java6-jdk' do
-    let(:params) { {} }
-
-    it { should contain_class("jira::package").with_require('Package[sun-java6-jdk]') }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/JIRA_MAX_PERM_SIZE=256m/) }
   end
 
   describe 'depends on custom java package' do
     let(:params) { {:java_package => 'custom-java-jdk'} }
 
-    it { should contain_class("jira::package").with_require('Package[custom-java-jdk]') }
-  end
-
-  describe 'by default' do
-    let(:params) { {} }
-
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').without_content(/-Datlassian.plugins.enable.wait=/)
-    end
+    specify { should contain_class("jira::package").with_require('Package[custom-java-jdk]') }
   end
 
   describe 'with plugin_startup_timeout => 600' do
     let(:params) { { :plugin_startup_timeout => 600} }
 
-    it do
-      should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/-Datlassian.plugins.enable.wait=600/)
-    end
-  end
-
-  describe 'configures cron job to clean up export directory' do
-    let(:params) { {} }
-
-    it { should contain_cron('cleanup-jira-export').with_command('find /data/jira/export/ -name "*.zip" -type f -mtime +7 -delete') }
-  end
-
-  describe 'creates database configuration in the data dir' do
-    let(:params) { {} }
-
-    it { should contain_file('/data/jira/dbconfig.xml') }
+    specify { should contain_file('/opt/atlassian-jira-6.2-standalone/bin/setenv.sh').with_content(/-Datlassian.plugins.enable.wait=600/) }
   end
 end
